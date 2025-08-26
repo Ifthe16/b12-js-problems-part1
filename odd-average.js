52e5f474558f3e11ca77d3bb636378495aaf3c9a
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-26 17:40:51 +0600

    added odd-average.js file

---
+++ odd-average.js
@@ -0,0 +1,28 @@
+/**
+ * function takes array as parameter
+ * give me the average of the odd numbers in the array
+*/
+
+/**
+ * 1. put odd numbers in an array*/
+
+function oddAverage(numbers) {
+    const odds = [];
+    for (const number of numbers) {
+        if (number % 2 === 1) {
+            odds.push(number);
+        }
+    }
+    // odds is the array that contains only the odd numbers
+    let sum = 0;
+    for (const number of odds) {
+        sum += number;
+    }
+    const count = odds.length;
+    const avg = sum / count;
+    return avg;
+}
+
+const numbers = [42, 13, 58, 65, 81, 7];
+const avg = oddAverage(numbers);
+console.log('average of the odd numbers is: ', avg);
