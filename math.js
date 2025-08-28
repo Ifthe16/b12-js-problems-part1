6986f12766234c3bc57e86db258700e3e7ffef60
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-28 16:33:26 +0600

    added math.js file

---
+++ math.js
@@ -0,0 +1,51 @@
+/* const Math = {
+    min: function min(num1, num2){
+        return 5;
+    }
+}
+
+console.log(Math.min); */
+
+const min = Math.min(45, 21, 65, 99, 126, 5, -99);
+console.log(min);
+
+const max = Math.max(45, 21, 65, 99, 126, 5, -99, 987);
+console.log(max);
+
+console.log(Math.PI);
+
+console.log(Math.abs(-88));
+console.log(Math.abs(88));
+console.log(Math.abs(10 - 5));
+console.log(Math.abs(5 - 10));
+
+console.log(Math.round(2.25));
+console.log(Math.round(2.85));
+
+console.log(Math.floor(2.95));
+console.log(Math.floor(2.9995));
+
+console.log(Math.ceil(2.95));
+console.log(Math.ceil(2.00001));
+console.log(Math.ceil(2.9995));
+
+console.log(Math.random());
+console.log(Math.random() * 10);
+
+const rand = Math.round(Math.random() * 10);
+console.log(rand);
+
+console.log(Math.pow(2, 3));
+console.log(Math.pow(3, 2));
+console.log(Math.pow(3, 3));
+
+console.log(Math.sqrt(4));
+console.log(Math.sqrt(144));
+
+console.log(Math.SQRT1_2);
+console.log(Math.SQRT2);
+
+
+
+
+
